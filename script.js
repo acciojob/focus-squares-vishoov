22fbbb4f2df7409f5092c82d7881709608ca4d27
Vishoo Verma <78697370+vishoov@users.noreply.github.com>
2026-04-23 12:06:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+//select squares
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((currentSquare)=>{
+	//mouseenter
+	currentSquare.addEventListener('mouseover', ()=>{
+		squares.forEach((curr)=>{
+			if(curr!==currentSquare){
+				
+			curr.style.backgroundColor = '#6F4E37'
+			}
+		})
+	})
+
+	currentSquare.addEventListener('mouseout', ()=>{
+		squares.forEach((el)=>el.style.backgroundColor='#E6E6FA')
+	})
+})
